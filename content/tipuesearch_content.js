6ad750af1f712813c9cb253ecd8bf32326786281
41023154 <41023154@gm.nfu.edu.tw>
2023-03-06 11:51:21 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2a-pj1ag15 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2a-pj1ag15 \n 123 \n', 'tags': '', 'url': 'About.html'}]};
