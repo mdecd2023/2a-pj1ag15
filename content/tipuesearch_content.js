eb4555f670e7962b5339fd54629f74582d12297d
41023135 <41023135@gm.nfu.edu.tw>
2023-03-06 11:55:10 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2a-pj1ag15', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2a-pj1ag15 \n e123', 'tags': '', 'url': 'About.html'}]};
